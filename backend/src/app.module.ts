12422e10b243203eb604f636aa3b3570517258c9
FernandoAlfonso <alfonso07amaya@gmail.com>
2025-07-09 13:31:48 -0500

    feat, create module and turn folders

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ModuleModule } from './module/module.module';
+import { TurnModule } from './turn/turn.module';
 
 @Module({
-  imports: [],
+  imports: [ModuleModule, TurnModule],
   controllers: [],
   providers: [],
 })
